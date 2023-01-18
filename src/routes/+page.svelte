f87bf8cb43b7a493df013caccfb2696ac1de4b95
GUGUcomet <83127928+GUGUcomet@users.noreply.github.com>
2023-01-18 22:30:09 +0900

    230118 작업중

---
+++ src/routes/+page.svelte
@@ -0,0 +1,69 @@
+
+<link href="https://fonts.googleapis.com/css?family=Manrope" rel="stylesheet" />
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+
+<article class="grid-wrap">
+  <div style="grid-item">
+    <div class="btn-wrap-full">
+      <button class="btn">
+        <span class="material-icons md-24">face</span>
+        <div class="btn-content">
+          <span class="btn-main-txt">Action</span>
+          <br />
+          <span class="btn-sub-txt">Descrddddiptioddn</span>
+        </div>
+        <span
+          class="material-icons md-24">face</span
+        >
+        <!-- svelte-ignore a11y-missing-attribute -->
+      </button>
+    </div>
+    <div class="btn-wrap-full">
+      <button class="btn medium">
+        <div class="btn-content">
+          <span class="btn-main-txt">Action</span>
+        </div>
+        <span class="material-icons md-24">face</span>
+        <!-- svelte-ignore a11y-missing-attribute -->
+      </button>
+      <button class="btn small">
+        <div class="btn-txt-wrap">
+          <span class="btn-main-txt">Action</span>
+        </div></button>
+    </div>
+    <div class="btn-wrap-full">
+      <button class="btn secondary btn-slide">
+        <span class="btn-main-txt">Action</span>
+      </button>
+    </div>
+    <div class="btn-wrap-full">
+       <button class="btn teritary btn-marquee">
+          <span class="btn-main-txt" data-text="marquee">marquee</span>
+        </button>
+        <button class="btn outline btn-offset">
+          <span class="btn-main-txt" data-text="marquee">marquee</span>
+        </button>
+    </div>
+  
+  
+  </div>
+  <div style="grid-item">
+    <div class="btn-wrap-full">
+      <button class="btn">
+        <span class="material-icons md-24">face</span>
+        <div class="btn-content">
+          <span class="btn-main-txt">Action</span>
+          <br />
+          <span class="btn-sub-txt">Descrddddiptioddn</span>
+        </div>
+        <span
+          class="material-icons md-24">face</span
+        >
+        <!-- svelte-ignore a11y-missing-attribute -->
+      </button>
+    </div>  
+  </div>
+
+</article>
+
+<object type="image/svg+xml" data="/src/img/ico/24/Attach.svg" class="svg-icon" />
